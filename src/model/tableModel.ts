5236f7225d601d648ae0378cfb924b87dc12f273
babii123 <2943867123@qq.com>
2024-04-18 00:46:01 +0800

    Modify the code structure

---
+++ src/model/tableModel.ts
@@ -0,0 +1,172 @@
+// 水资源类型表格数据类型
+export interface WaterTypeTableType {
+  key: number
+  id: number
+  type: string
+  description: string
+}
+
+export interface WaterType {
+  id: number
+  type: string
+  description: string
+}
+
+// 水资源表格数据类型
+export interface WaterTableType {
+  key: number
+  id: number
+  type: string
+  waterName: string
+  address: string
+  description: string
+  addTime: Date
+  addUser: string
+  checkUser: string[]
+  isDel: boolean
+  delReason: string
+}
+
+export interface WaterData {
+  id: number
+  type: string
+  waterName: string
+  address: string
+  description: string
+  addTime: Date
+  addUser: string
+  checkUser: string[]
+  isDel: boolean
+  delReason: string
+}
+
+// 水量表格数据类型
+export interface WaterStorageTableType {
+  key?: number
+  id: number
+  resourceId: number;
+  addTime?: Date;
+  addUser?: string
+  detectTime?: Date;
+  detectPeople?: string[];
+  supply?: number;
+  storage?: number;
+  isDel?: boolean;
+  delReason?: string;
+}
+
+export interface WaterStorageData {
+  id: number
+  resourceId: number;
+  addTime: Date;
+  addUser: string
+  detectTime: Date;
+  detectPeople: string[];
+  supply: number;
+  storage: number;
+  isDel: boolean;
+  delReason: string;
+}
+
+// 水质表格数据类型
+export interface WaterQualityTableType {
+  key?: number
+  id: number
+  resourceId: number
+  addTime?: Date
+  addUser?: string
+  detectTime?: Date
+  detectPeople?: string[]
+  ph?: number
+  turbidity?: number
+  fluoride?: number
+  cyanin?: number
+  isDel?: boolean
+  delReason?: string
+}
+
+export interface WaterQualityData {
+  id: number
+  resourceId: number
+  addTime: Date
+  addUser: string
+  detectTime: Date
+  detectPeople: string[]
+  ph: number
+  turbidity: number
+  fluoride: number
+  cyanin: number
+  isDel: boolean
+  delReason: string
+}
+
+// 水价表格数据类型
+export interface WaterPriceTableType {
+  key: number
+  id: number
+  type: string
+  basicPrice: number
+  resourceCost: number
+  pollutionCost: number
+  realPrice: number
+}
+
+export interface WaterPriceData {
+  id: number
+  type: string
+  basicPrice: number
+  resourceCost: number
+  pollutionCost: number
+  realPrice: number
+}
+
+// 供水计划表格数据类型
+export interface PlanTableType {
+  key?: number
+  id: number
+  addTime: Date | any
+  startTime: Date | any
+  endTime: Date | any
+  waterSources: string[]
+  waterArea: string
+  waterPriceType: string
+  description: string
+  addUser: string
+  isDel: boolean
+  delReason: string
+}
+// Create
+export interface PlanData {
+  id: number
+  addTime: Date
+  startTime: Date
+  endTime: Date
+  waterSources: string[]
+  waterArea: string
+  waterPriceType: string
+  description: string
+  addUser: string
+  isDel: boolean
+  delReason: string
+}
+
+export interface WaterLinkData {
+  id: number;
+  title: string;
+  link: string;
+}
+
+export interface NoticeListType {
+  key?: number
+  id: number
+  addTime: Date | any
+  startTime: Date | any
+  endTime: Date | any
+  waterSources: string[]
+  waterArea: string
+  waterPriceType: string
+  description: string
+  addUser: string
+  isDel: boolean
+  delReason: string
+}
---
+++ src/store/actionTypes/actionTypes.ts
@@ -0,0 +1,85 @@
+const noticeBasicAction = 'notice_list'
+
+export const UPDATE_NOTICE_LIST = `${noticeBasicAction}/update_notice`
+export const GET_NOTICE_LIST_BYAPI = `${noticeBasicAction}/get_notice_list_byAPI`
+export const CREATE_NOTICE = `${noticeBasicAction}/create_notice`
+export const DELETE_NOTICE = `${noticeBasicAction}/delete_notice`
+
+const planBasicAction = 'plan'
+
+export const UPDATE_PLAN_LIST = `${planBasicAction}/update_plan_list`
+export const GET_PLAN_LIST_BYAPI = `${planBasicAction}/get_plan_byAPI`
+export const CREATE_PLAN = `${planBasicAction}/create_plan`
+export const UPDATE_PLAN = `${planBasicAction}/update_plan`
+export const DELETE_PLAN = `${planBasicAction}/delete_plan`
+export const DELETE_PLAN_BY_REASON = `${planBasicAction}/delete_plan_by_reason`
+export const DELETE_PLAN_LIST = `${planBasicAction}/delete_plan_list`
+export const FIND_PLAN_BY_CONDITION = `${planBasicAction}/find_plan_by_condition`
+
+const userBasicAction = 'user'
+
+export const SET_ROLES = `${userBasicAction}/set_roles`
+export const CHANGE_MENU_ITEMS = `${userBasicAction}/change_menu_items`
+export const UPDATE_USER_INFO = `${userBasicAction}/update_user_info`
+export const UPDATE_USER_INFO_TO_SERVER = `${userBasicAction}/update_user_info_to_server`
+export const GET_USER_INFO = `${userBasicAction}/get_user_info`
+
+const userListBasicAction = 'user_list'
+
+export const UPDATE_USER_LIST = `${userListBasicAction}/update_user_list`
+export const GET_USER_LIST_BYAPI = `${userListBasicAction}/get_user_list_byAPI`
+export const CREATE_USER = `${userListBasicAction}/create_user`
+export const UPDATE_USER = `${userListBasicAction}/update_user`
+export const DELETE_USER = `${userListBasicAction}/delete_user`
+export const DELETE_USER_LIST = `${userListBasicAction}/delete_user_list`
+export const FIND_USER_BY_CONDITION = `${userListBasicAction}/find_user_by_condition`
+
+const waterListBasicAction = 'water_list'
+
+export const UPDATE_WATER_LIST = `${waterListBasicAction}/update_water_list`
+export const GET_WATER_LIST_BYAPI = `${waterListBasicAction}/get_water_list_byAPI`
+export const CREATE_WATER = `${waterListBasicAction}/create_water`
+export const UPDATE_WATER = `${waterListBasicAction}/update_water`
+export const DELETE_WATER = `${waterListBasicAction}/delete_water`
+export const DELETE_WATER_BY_REASON = `${waterListBasicAction}/delete_water_by_reason`
+export const DELETE_WATER_LIST = `${waterListBasicAction}/delete_water_list`
+export const FIND_WATER_BY_CONDITION = `${waterListBasicAction}/find_water_by_condition`
+
+const waterPriceListBasicAction = 'water_price'
+
+export const UPDATE_WATER_PRICE_LIST = `${waterPriceListBasicAction}/update_water_price_list`
+export const GET_WATER_PRICE_LIST_BYAPI = `${waterPriceListBasicAction}/get_water_price_list_byAPI`
+
+const waterTypeListBasicAction = 'water_type_list'
+
+export const UPDATE_WATER_TYPE_LIST = `${waterTypeListBasicAction}/update_water_type_list`
+export const GET_WATER_TYPE_LIST_BYAPI = `${waterTypeListBasicAction}/get_water_type_list_byAPI`
+export const CREATE_WATER_TYPE = `${waterTypeListBasicAction}/create_water_type`
+export const UPDATE_WATER_TYPE = `${waterTypeListBasicAction}/update_water_type`
+export const DELETE_WATER_TYPE = `${waterTypeListBasicAction}/delete_water_type`
+export const DELETE_WATER_TYPE_LIST = `${waterTypeListBasicAction}/delete_water_type_list`
+
+
+const waterStorageListBasicAction = 'water_storage'
+
+export const UPDATE_WATER_STORAGE_LIST = `${waterStorageListBasicAction}/update_water_storage_list`
+export const GET_WATER_STORAGE_LIST_BYAPI = `${waterStorageListBasicAction}/get_water_storage_list_byAPI`
+export const CREATE_WATER_STORAGE = `${waterStorageListBasicAction}/create_water_storage`
+export const UPDATE_WATER_STORAGE = `${waterStorageListBasicAction}/update_water_storage`
+export const DELETE_WATER_STORAGE = `${waterStorageListBasicAction}/delete_water_storage`
+export const DELETE_WATER_STORAGE_BY_REASON = `${waterStorageListBasicAction}/delete_water_storage_by_reason`
+export const DELETE_WATER_STORAGE_LIST = `${waterStorageListBasicAction}/delete_water_storage_list`
+export const FIND_STORAGE_BY_ID = `${waterStorageListBasicAction}/find_storage_by_id`
+
+const waterqualityListBasicAction = 'water_quality'
+
+export const UPDATE_WATER_QUALITY_LIST = `${waterqualityListBasicAction}/update_water_quality_list`
+export const GET_WATER_QUALITY_LIST_BYAPI = `${waterqualityListBasicAction}/get_water_quality_list_byAPI`
+export const CREATE_WATER_QUALITY = `${waterqualityListBasicAction}/create_water_quality`
+export const UPDATE_WATER_QUALITY = `${waterqualityListBasicAction}/update_water_quality`
+export const DELETE_WATER_QUALITY = `${waterqualityListBasicAction}/delete_water_quality_multi`
+export const DELETE_WATER_QUALITY_BY_REASON = `${waterqualityListBasicAction}/delete_water_quality_by_reason`
+export const DELETE_WATER_QUALITY_LIST = `${waterqualityListBasicAction}/delete_water_quality_list`
+export const FIND_QUALITY_BY_ID = `${waterqualityListBasicAction}/find_quality_by_id`
+
+
